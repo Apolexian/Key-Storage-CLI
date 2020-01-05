640f8a2248e48556f00dce9adafb6cd574c44ebc
Apolexian <ivananikitin98@gmail.com>
2020-01-05 17:45:27 +0000

    started work on file backed system, added mutex to Vault type for thread safety

--- internal/vault/storage.go
+++ internal/vault/storage.go
@@ -2,42 +2,72 @@ package vault
 
 import (
 	"../encryption"
+	"encoding/json"
 	"errors"
+	"os"
+	"sync"
 )
 
 // Vault is used to store the encoded keys
 type Vault struct {
 	encodingKey string
+	filepath    string
+	mutex       sync.Mutex
 	keyValues   map[string]string
 }
 
 // Memory will map the encoding string and wrap it into a Vault struct
-func Memory(encodingKey string) Vault {
-	return Vault{encodingKey: encodingKey,
-		keyValues: make(map[string]string)}
+func File(encodingKey, filepath string) *Vault {
+	return &Vault{encodingKey: encodingKey,
+		keyValues: make(map[string]string),
+		filepath:  filepath}
+}
+
+// getKeyValues returns the values from the json file
+// if not present makes new one instead of error
+func (v *Vault) getKeyValues() error {
+	f, err := os.Open(v.filepath)
+	if err != nil {
+		v.keyValues = make(map[string]string)
+		return nil
+	}
+	decoder := json.NewDecoder(f)
+	err = decoder.Decode(&v.keyValues)
+	if err != nil {
+		return err
+	}
+	return nil
 }
 
 // Get returns the decrypted version of the key we need
 func (v *Vault) Get(key string) (string, error) {
-	hex, ok := v.keyValues[key]
-	if !ok {
-		return "", errors.New("no key value found")
-	}
-	ret, err := encryption.Decrypt(v.encodingKey, hex)
+	v.mutex.Lock()
+	defer v.mutex.Unlock()
+	err := v.getKeyValues()
 	if err != nil {
 		return "", err
 	}
 
-	return ret, nil
+	value, ok := v.keyValues[key]
+	if !ok {
+		return "", errors.New("no key value found")
+	}
+	return value, nil
 }
 
 // Set will load encrypted key into memory
 func (v *Vault) Set(key, value string) error {
+	v.mutex.Lock()
+	defer v.mutex.Unlock()
 	encryptedValue, err := encryption.Encrypt(v.encodingKey, value)
 	if err != nil {
 		return err
 	}
-
+	err = v.getKeyValues()
+	if err != nil {
+		return err
+	}
 	v.keyValues[key] = encryptedValue
-	return nil
+	err = v.putKeyValues()
+	return err
 }
