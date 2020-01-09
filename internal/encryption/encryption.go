1b0e70ccf744e6f76bdd58710ecfd4ceb8f93454
Apolexian <ivananikitin98@gmail.com>
2020-01-09 15:02:30 +0000

    changed encryption to use an encrypted io writer stream

--- internal/encryption/encryption.go
+++ internal/encryption/encryption.go
@@ -11,26 +11,48 @@ import (
 	"io"
 )
 
+func encryptStream(key string, iv []byte) (cipher.Stream, error) {
+	block, err := createCipher(key)
+	if err != nil {
+		return nil, err
+	}
+	return cipher.NewCFBEncrypter(block, iv), nil
+}
+
 // Encrypt takes in a key and plaintext and returns the hex representation
 // of the the encrypted value
 // code is based on standard library examples at:
 // https://golang.org/pkg/crypto/cipher/#NewCFBEncrypter
 func Encrypt(key, plaintext string) (string, error) {
-	block, err := createCipher(key)
-	if err != nil {
-		return "", err
-	}
-
 	ciphertext := make([]byte, aes.BlockSize+len(plaintext))
 	iv := ciphertext[:aes.BlockSize]
 	if _, err := io.ReadFull(rand.Reader, iv); err != nil {
 		return "", err
 	}
-
-	stream := cipher.NewCFBEncrypter(block, iv)
+	stream, err := encryptStream(key, iv)
+	if err != nil {
+		return "", err
+	}
 	stream.XORKeyStream(ciphertext[aes.BlockSize:], []byte(plaintext))
-
 	return fmt.Sprintf("%x", ciphertext), nil
+}
+
+// EncryptWriter passes a writer to make sure that whatever
+// is written to the file is encrypted
+func EncryptWriter(key string, w io.Writer) (*cipher.StreamWriter, error) {
+	iv := make([]byte, aes.BlockSize)
+	if _, err := io.ReadFull(rand.Reader, iv); err != nil {
+		return nil, err
+	}
+	stream, err := encryptStream(key, iv)
+	if err != nil {
+		return nil, err
+	}
+	n, err := w.Write(iv)
+	if n != len(iv) || err != nil {
+		return nil, errors.New("encrypt writer not able to write full iv to writer")
+	}
+	return &cipher.StreamWriter{S: stream, W: w}, nil
 }
 
 // Decrypt takes in a key and the encrypted text(hex) and decrypt it
@@ -46,15 +68,12 @@ func Decrypt(key, encryptedText string) (string, error) {
 	if err != nil {
 		return "", err
 	}
-
 	if len(ciphertext) < aes.BlockSize {
 		return "", errors.New("encrypt: cipher is too short")
 	}
 	iv := ciphertext[:aes.BlockSize]
 	ciphertext = ciphertext[aes.BlockSize:]
-
 	stream := cipher.NewCFBDecrypter(block, iv)
-
 	stream.XORKeyStream(ciphertext, ciphertext)
 	return string(ciphertext), nil
 
