8dce1fb046eef8b11a54c70e02ece5b99e79631d
Apolexian <ivananikitin98@gmail.com>
2020-01-17 12:50:11 +0000

    re-wrote encryptionStream_test.go to use testLogger

--- test/encryptionStream_test.go
+++ test/encryptionStream_test.go
@@ -1,56 +1,53 @@
 package test
 
 import (
+	logger "../internal/logger"
 	"../internal/storage"
-	"log"
-	"os"
-	"path/filepath"
 	"testing"
 )
 
 func TestEncryptStream(t *testing.T) {
-	logPath, _ := filepath.Abs("../logs/EncryptionStreamTest_Log")
-	f, err := os.OpenFile(logPath, os.O_RDWR|os.O_CREATE, 0666)
-	if err != nil {
-		log.Fatalf("error opening file: %v", err)
-	}
-	defer f.Close()
-	log.SetOutput(f)
 	v := storage.File("stream-my-key", "stream.secrets")
 
-	err = v.Set("stream-key1", "test-value-1")
-	log.Printf("Set stream-key1")
+	err := v.Set("stream-key1", "test-value-1")
+	logger.TestLogger.Printf("Set stream-key1")
 	if err != nil {
 		t.Errorf("failed to set: %s", err)
+		logger.TestLogger.Printf("failed to set: %s", err)
 	}
 
 	err = v.Set("stream-key2", "test-value-2")
-	log.Printf("Set stream-key2")
+	logger.TestLogger.Printf("Set stream-key2")
 	if err != nil {
 		t.Errorf("failed to set: %s", err)
+		logger.TestLogger.Printf("failed to set: %s", err)
 	}
 
 	err = v.Set("stream-key3", "test-value-3")
-	log.Printf("Set stream-key3")
+	logger.TestLogger.Printf("Set stream-key3")
 	if err != nil {
 		t.Errorf("failed to set: %s", err)
+		logger.TestLogger.Printf("failed to set: %s", err)
 	}
 
 	plain, err := v.Get("stream-key1")
 	if err != nil {
 		t.Errorf("failed to get: %s", err)
+		logger.TestLogger.Printf("failed to get: %s", err)
 	}
-	log.Printf("Got plain: %s", plain)
+	logger.TestLogger.Printf("Got plain: %s", plain)
 
 	plain, err = v.Get("stream-key2")
 	if err != nil {
 		t.Errorf("failed to get: %s", err)
+		logger.TestLogger.Printf("failed to get: %s", err)
 	}
-	log.Printf("Got plain: %s", plain)
+	logger.TestLogger.Printf("Got plain: %s", plain)
 
 	plain, err = v.Get("stream-key3")
 	if err != nil {
 		t.Errorf("failed to get: %s", err)
+		logger.TestLogger.Printf("failed to get: %s", err)
 	}
-	log.Printf("Got plain: %s", plain)
+	logger.TestLogger.Printf("Got plain: %s", plain)
 }
