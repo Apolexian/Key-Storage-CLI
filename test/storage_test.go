13af17a1053fd6846ef8b5846fb04b88ed4547b1
Apolexian <ivananikitin98@gmail.com>
2020-01-17 13:09:55 +0000

    storage_test.go now uses testLogger

--- test/storage_test.go
+++ test/storage_test.go
@@ -1,74 +1,65 @@
 package test
 
 import (
+	logger "../internal/logger"
 	"../internal/storage"
-	"log"
-	"os"
-	"path/filepath"
 	"testing"
 )
 
 func TestStorage(t *testing.T) {
-	logPath, _ := filepath.Abs("../logs/StorageTest_Log")
-	f, err := os.OpenFile(logPath, os.O_RDWR|os.O_CREATE, 0666)
-	if err != nil {
-		log.Fatalf("error opening file: %v", err)
-	}
-	defer f.Close()
-	log.SetOutput(f)
 	v := storage.File("test-key", "storage.secrets")
 
-	err = v.Set("key1", "test-value-1")
-	log.Printf("Set key1")
+	err := v.Set("key1", "test-value-1")
+	logger.TestLogger.Printf("Set key1")
 	if err != nil {
 		t.Errorf("failed to set: %s", err)
-		log.Printf("error on setting key1: %s", err)
+		logger.TestLogger.Printf("error on setting key1: %s", err)
 	}
 
 	err = v.Set("key2", "test-value-2")
-	log.Printf("Set key2")
+	logger.TestLogger.Printf("Set key2")
 	if err != nil {
 		t.Errorf("failed to set: %s", err)
-		log.Printf("error on setting key2: %s", err)
+		logger.TestLogger.Printf("error on setting key2: %s", err)
 	}
 
 	err = v.Set("key3", "test-value-3")
-	log.Printf("Set key3")
+	logger.TestLogger.Printf("Set key3")
 	if err != nil {
 		t.Errorf("failed to set: %s", err)
-		log.Printf("error on setting key3: %s", err)
+		logger.TestLogger.Printf("error on setting key3: %s", err)
 	}
 
 	plain, err := v.Get("key1")
-	log.Printf("Got plain: %s", plain)
+	logger.TestLogger.Printf("Got plain: %s", plain)
 	if err != nil {
 		t.Errorf("failed to get demo key with error: %s", err)
-		log.Printf("error on getting key1: %s", err)
+		logger.TestLogger.Printf("error on getting key1: %s", err)
 	}
 	if plain != "test-value-1" {
 		t.Errorf("expected test-value-1, got: %s", plain)
-		log.Printf("expected test-value-1, got: %s", plain)
+		logger.TestLogger.Printf("expected test-value-1, got: %s", plain)
 	}
 
 	plain, err = v.Get("key2")
-	log.Printf("Got plain: %s", plain)
+	logger.TestLogger.Printf("Got plain: %s", plain)
 	if err != nil {
 		t.Errorf("failed to get demo key with error: %s", err)
-		log.Printf("error on getting key2: %s", err)
+		logger.TestLogger.Printf("error on getting key2: %s", err)
 	}
 	if plain != "test-value-2" {
 		t.Errorf("expected test-value-2, got: %s", plain)
-		log.Printf("expected test-value-2, got: %s", plain)
+		logger.TestLogger.Printf("expected test-value-2, got: %s", plain)
 	}
 
 	plain, err = v.Get("key3")
-	log.Printf("Got plain: %s", plain)
+	logger.TestLogger.Printf("Got plain: %s", plain)
 	if err != nil {
 		t.Errorf("failed to get demo key with error: %s", err)
-		log.Printf("error on getting key3: %s", err)
+		logger.TestLogger.Printf("error on getting key3: %s", err)
 	}
 	if plain != "test-value-3" {
 		t.Errorf("expected test-value-3, got: %s", plain)
-		log.Printf("expected test-value-3, got: %s", plain)
+		logger.TestLogger.Printf("expected test-value-3, got: %s", plain)
 	}
 }
